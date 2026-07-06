7f7e42275e0300be3fecccb5479592b2976786f2
Metin Yildiztas <aderici@unc.edu>
2026-07-05 21:42:32 -0400

    Add validateInput and validateEmail functions for input validation

--- ContactForm.php
+++ ContactForm.php
@@ -7,6 +7,37 @@
 
 <?php
 
+function validateInput($data, $fieldName) {
+    global $errorCount;
+    if (empty($data)) {
+        echo "\"$fieldName\" is a required field.<br />\n";
+        ++$errorCount;
+        $retval = "";
+    }
+    else {
+        $retval = trim($data);
+        $retval = stripslashes($retval);
+    }
+    return($retval);
+}
+
+function validateEmail($data, $fieldName) {
+    global $errorCount;
+
+    if (empty($data)) {
+        echo "\"$fieldName\" is a required field.<br />\n";
+        ++$errorCount; $retval = "";
+    }
+    else {
+        $retval = filter_var($data, FILTER_SANITIZE_EMAIL);
+
+        if (!filter_var($retval, FILTER_VALIDATE_EMAIL)) {
+            echo "\"$fieldName\" is not a valid e-mail address.<br />\n";
+        }
+    }
+    return($retval);
+}
+
 ?>
 
 </body>
